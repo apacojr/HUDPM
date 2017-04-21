f9fda0b479e5ef0533df2f2c52caa2aa0c65cba5
Jon <apacojr@hotmail.com>
2017-04-22 03:21:12 +0800

    Add project files.

---
+++ HUDPM/Demo.js
@@ -0,0 +1,161 @@
+﻿/// <reference path="C:\Users\Jon\Desktop\HUDPM\HUDPM\HUDPM\Scripts/angular.js" />
+
+var MyApp = angular.module("MyApp", ['ngRoute', 'EmployeeService']);
+
+MyApp.config(['$routeProvider', function ($routeProvider)
+{
+    $routeProvider.when('/Add',
+        {
+            templateUrl: 'Views/add.html',
+            controller: 'AddController'
+        })
+        .when('/Edit',
+        {
+            templateUrl: 'Views/edit.html',
+            controller: 'EditController'
+        })
+        .when('/Delete',
+        {
+            templateUrl: 'Views/delete.html',
+            controller: 'DeleteController'
+        })
+        .when('/Home',
+        {
+            templateUrl: 'Views/home.html',
+            controller: 'HomeController'
+        })
+        .otherwise({
+            redirectTo: '/Home'
+        });
+}
+]);
+
+MyApp.controller("AddController", function($scope, EmpApi) {
+       $scope.addEmp = function() {
+           var empToAdd = {
+               'Name': $scope.name,
+               'Age': $scope.age,
+               'Salary': $scope.sal
+           };
+
+           EmpApi.AddEmployee(empToAdd)
+               .then(function(response) {
+                   alert("user added");
+
+                   $scope.name = undefined;
+                   $scope.age = undefined;
+                   $scope.sal = undefined;
+
+               });
+       }
+    });
+
+MyApp.controller("DeleteController", function ($scope, EmpApi) {
+    $scope.selectedItem = "Select Employee";
+    $scope.isDeleteItemVisible = false;
+
+    getEmployees();
+
+    function getEmployees() {
+        EmpApi.getEmployees()
+            .then(function (result) {
+                $scope.emps = result.data;
+            });
+    }
+
+    $scope.dropboxitemselected = function (item) {
+        $scope.isDeleteItemVisible = true;
+        $scope.selectedItem = item.Id;
+        $scope.name = item.Name;
+        $scope.age = item.Age;
+        $scope.sal = item.Salary;
+        $scope.empid = item.Id;
+    }
+
+    $scope.DeleteEmp = function() {
+        var empToDelete = {
+            'Id': $scope.empid
+        };
+    
+    EmpApi.DeleteEmployee(empToDelete)
+            .then(function (response) {
+                alert("user deleted");
+
+                $scope.name = undefined;
+                $scope.age = undefined;
+                $scope.sal = undefined;
+                $scope.empid = undefined;
+                $scope.selectedItem = "Select Employee";
+                $scope.isDeleteItemVisible = false;
+                getEmployees();
+
+            });
+    }
+
+    });
+
+MyApp.controller("EditController", function ($scope, EmpApi) {
+    $scope.selectedItem = "Select Employee";
+    $scope.isDeleteItemVisible = false;
+
+    getEmployees();
+
+    function getEmployees() {
+        EmpApi.getEmployees()
+            .then(function(result) {
+                $scope.emps = result.data;
+            });
+    }
+
+    $scope.dropboxitemselected = function(item) {
+        $scope.isDeleteItemVisible = true;
+        $scope.selectedItem = item.Id;
+        $scope.name = item.Name;
+        $scope.age = item.Age;
+        $scope.sal = item.Salary;
+        $scope.empid = item.Id;
+    }
+
+    $scope.UpdateEmp = function() {
+        var empToUpdate = {
+            'Id': $scope.empid,
+            'Name': $scope.name,
+            'Age': $scope.age,
+            'Salary': $scope.sal
+        };
+
+        EmpApi.EditEmployee(empToUpdate)
+            .then(function(response) {
+                alert("user updated");
+
+                $scope.name = undefined;
+                $scope.age = undefined;
+                $scope.sal = undefined;
+                $scope.empid = undefined;
+                $scope.selectedItem = "Select Employee";
+                $scope.isDeleteItemVisible = false;
+                getEmployees();
+
+            });
+    }
+
+});
+
+MyApp.controller("HomeController", function ($scope, EmpApi) {
+
+    getEmployee();
+    
+    function getEmployee() {
+        EmpApi.getEmployees()
+            .then(function(result) {
+                $scope.emps = result.data;
+            });
+        //.then(function (error) {
+        //    $scope.status = 'Unable to load emp data: ' + error.message;
+        //});
+    }
+
+
+    });
+
+
---
+++ HUDPM/service.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="C:\Users\Jon\Desktop\HUDPM\HUDPM\HUDPM\Scripts/angular.js" />
+
+var EmployeeService = angular.module('EmployeeService', []);
+
+EmployeeService.factory('EmpApi', function($http) {
+
+    var urlBase = "http://localhost:54455/api";
+    var EmpApi = {};
+
+    // GET
+    EmpApi.getEmployees = function() {
+        return $http.get(urlBase + '/Employees');
+    };
+
+    // ADD
+    EmpApi.AddEmployee = function(emp) {
+        return $http.post(urlBase + '/Employees/', emp);
+    }
+
+    // EDIT
+    EmpApi.EditEmployee = function(empToUpdate) {
+       return $http.put(urlBase + '/Employees/'+ empToUpdate.Id, empToUpdate );
+    }
+    
+    // DELETE
+    EmpApi.DeleteEmployee = function (empToDelete) {
+        return $http.delete(urlBase + '/Employees/' + empToDelete.Id);
+    }
+
+    return EmpApi;
+});
